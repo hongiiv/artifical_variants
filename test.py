7e56fd12f6b959e30d0ef09e775dfeeb2380e225
hongiiv <hongiiv@gmail.com>
2018-10-19 11:30:47 +0900

    test case update

--- test.py
+++ test.py
@@ -1,10 +1,14 @@
 import unittest
+import artifical_variants
 
 def get_helloworld():
     return 'hello world'
 
 def test_get_helloworld():
     assert 'hello world' == get_helloworld()
+
+def test_deletions():
+    assert [{'ALT': 'A', 'REF': 'A', 'POS': 1111111, '#CHROM': 1}, {'ALT': 'A', 'REF': 'A', 'POS': 1111111, '#CHROM': 1}, {'ALT': 'A', 'REF': 'A', 'POS': 1111111, '#CHROM': 1}] == artifical_variants.get_deletions(1,1111111,'A')
 
 def main():
     print(get_helloworld())
